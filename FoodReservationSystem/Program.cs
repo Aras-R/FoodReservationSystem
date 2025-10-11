b0817473befce9c46e2b04aeaf0fac8de09c858e
aras <arasrasoolzadeh@gmail.com>
2025-10-11 14:49:06 +0330

    Sertting connection string and dbcontext in program class

--- FoodReservationSystem/Program.cs
+++ FoodReservationSystem/Program.cs
@@ -1,29 +1,45 @@
+using FoodReservation.Application.Interfaces.Contexts;
+using FoodReservation.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+// ??? Connection String ?? ???? ??????
+var connectionString = "@\"Data Source=DESKTOP-8ILS0U2; Initial Catalog=FoodReservationDB; Integrated Security=True; TrustServerCertificate=True;";
+
+// ??? DbContext ? Interface
+builder.Services.AddDbContext<DatabaseContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
+builder.Services.AddScoped<IDatabaseContext, DatabaseContext>();
+
+// ????????? MVC
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// ??????? Middleware ? Routing
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles(); 
 app.UseRouting();
-
 app.UseAuthorization();
+
 
 app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+).WithStaticAssets();
 
 
 app.Run();
