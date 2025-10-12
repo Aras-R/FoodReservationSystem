f67a1af3ac7187be1fc6e66bd94991435a06ba39
aras <arasrasoolzadeh@gmail.com>
2025-10-12 10:42:14 +0330

    Writing the initial code of authentication

---
+++ FoodReservationSystem/Controllers/AuthenticationController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodReservationSystem.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignIn(string StudentNumber, string PassWord) 
+        {
+            if (StudentNumber == "123" && PassWord == "123")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        ViewBag.Error = "شماره دانشجویی یا رمز اشتباه است!";
+        return View();
+
+        }
+
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(string StudentNumber, string FullName, string PassWord)
+        {
+            return RedirectToAction("SignIn");
+        }
+    }
+}
--- FoodReservationSystem/Program.cs
+++ FoodReservationSystem/Program.cs
@@ -36,7 +36,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}"
+    pattern: "{controller=Authentication}/{action=SignIn}/{id?}"
 ).WithStaticAssets();
 
 
