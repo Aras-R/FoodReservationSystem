f2c544c15c410de15901817388021fbbfcb3c071
aras <arasrasoolzadeh@gmail.com>
2025-10-14 15:53:42 +0330

    Users List : Interface, service class and Dto class

---
+++ FoodReservation.Application/Interfaces/Users/Queries/IIGetUserService.cs
@@ -0,0 +1,15 @@
+﻿using FoodReservation.Application.Services.Users.Queries;
+using FoodReservation.Common.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodReservation.Application.Interfaces.Users.Queries
+{
+    public interface IGetUserService
+    {
+        ResultDto<List<GetUserDto>> Execute();
+    }
+}
---
+++ FoodReservation.Application/Services/Users/Queries/GetUserService.cs
@@ -0,0 +1,40 @@
+﻿using FoodReservation.Application.Interfaces.Contexts;
+using FoodReservation.Application.Interfaces.Users.Queries;
+using FoodReservation.Common.Dto;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FoodReservation.Application.Services.Users.Queries
+{
+    public class GetUserService : IGetUserService
+    {
+        private readonly IDatabaseContext _databaseContext;
+        public GetUserService(IDatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+        public ResultDto<List<GetUserDto>> Execute()
+        {
+            var users = _databaseContext.Users
+                .Select(u => new GetUserDto
+                {
+                    Id = u.Id,
+                    StudentNumber = u.StudentNumber,
+                    FullName = u.FullName,
+                    Role = u.Role.ToString()
+                })
+                .OrderBy(u => u.FullName)
+                .ToList();
+
+            return ResultDto<List<GetUserDto>>.Success(users, "لیست کاربران با موفقیت دریافت شد ✅");
+        }
+    }
+    public class GetUserDto
+    {
+        public int Id { get; set; }
+        public string StudentNumber { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; }
+    }
+}
