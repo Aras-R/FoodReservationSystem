6d57f45fd13da114f78456fb93fb7e48897547a9
aras <arasrasoolzadeh@gmail.com>
2025-11-01 10:42:46 +0330

    List of foods: controller, view and CSS

---
+++ FoodReservationSystem/Controllers/FoodController.cs
@@ -0,0 +1,26 @@
+﻿using FoodReservation.Application.Interfaces.FacadePatterns.FoodFacade;
+using FoodReservation.Application.Interfaces.Foods.Queries;
+using FoodReservation.Application.Services.Foods.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodReservationSystem.Controllers
+{
+    public class FoodController : Controller
+    {
+
+        //[Area("User")]
+        private readonly IFoodsFacade _foodsFacade;
+        public FoodController(IFoodsFacade foodsFacade)
+        {
+            _foodsFacade = foodsFacade;
+        }
+        public IActionResult List()
+        {
+            var result = _foodsFacade.GetFoodService.Execute();
+            if (!result.IsSuccess)
+                TempData["Error"] = result.Message;
+
+            return View(result.Data);
+        }
+    }
+}
